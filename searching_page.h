fd8f37343b48b7c1538113b6529fbc17fe7dc56c
francoso <francoso@users.noreply.github.com>
2020-05-28 01:09:50 +0800

    product页面差不多了
    给两个order加上了product的触发器，可以实时更新amount，但是还没msg弄报错。
    然后product页面发现bug是取数据不能单独取，一个为NULL就全取不出来。服了
    弄了个增加的transaction在product新增里面，但是还要删除的transaction
    还有视图就差不多了。bug凑合凑合吧

--- searching_page.h
+++ searching_page.h
@@ -16,10 +16,19 @@ class searching_page : public QMainWindow
     int current_client_ID;
     int current_shipment_ID;
     bool is_add;
+    int max_row_client;
 
     int current_order_ID_2;
     int current_factory_ID;
     bool is_add_2;
+    int max_row_factory;
+
+    int current_product_ID;
+    QString current_type;
+    int current_factory_ID_2;
+    bool is_add_3;
+    int max_row_product;
+
     Q_OBJECT
 
 public:
@@ -54,6 +63,18 @@ private slots:
     void on_pushButton_7_clicked();
 
     void on_new_button_2_clicked();
+
+    void on_product_enter_button_clicked();
+
+    void on_Product_List_itemSelectionChanged();
+
+    void on_p3_change_btn_clicked();
+
+    void on_new_button_3_clicked();
+
+    void on_p3_return_btn_clicked();
+
+    void on_p3_check_btn_clicked();
 
 private:
     Ui::searching_page *ui;
